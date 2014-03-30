7a1e72ef93764d56e2d72407d022ad9df37c4675
Ronen Teva <ronenteva@gmail.com>
2014-03-30 23:59:32 +0300

    changed session secret

--- config/session.js
+++ config/session.js
@@ -15,7 +15,7 @@ module.exports.session = {
     // Session secret is automatically generated when your new app is created
     // Replace at your own risk in production-- you will invalidate the cookies of your users,
     // forcing them to log in again.
-    secret: '01bc128006521e2fd8eb7c66e5882849',
+    secret: 'g4YCmuCQ0CPaSRHUIzpjenngSDMssQ9O',
 
 
     // Set the session cookie expire time
