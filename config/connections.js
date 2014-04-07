82a9296afef52d2802aae217d12ef13242d8b9fb
Ronen Teva <ronenteva@gmail.com>
2014-04-07 20:56:45 +0300

    cleaning

--- config/connections.js
+++ config/connections.js
@@ -19,62 +19,13 @@
  */
 
 module.exports.connections = {
-
-  // Local disk storage for DEVELOPMENT ONLY
-  //
-  // Installed by default.
-  //
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
-
-  // MySQL is the world's most popular relational database.
-  // http://en.wikipedia.org/wiki/MySQL
-  //
-  // Run:
-  // npm install sails-mysql
-  //
   localMysqlServer: {
     adapter : 'sails-mysql',
     host    : 'localhost',
     user    : 'myskills',
     password: 'nK]GTa3*/*Es25',
     database: 'myskills'
-  },
-
-  // MongoDB is the leading NoSQL database.
-  // http://en.wikipedia.org/wiki/MongoDB
-  //
-  // Run:
-  // npm install sails-mongo
-  //
-  someMongodbServer: {
-    adapter   : 'sails-mongo',
-    host      : 'localhost',
-    port      : 27017,
-    user      : 'username',
-    password  : 'password',
-    database  : 'your mongo db name here'
-  },
-
-  // PostgreSQL is another officially supported relational database. 
-  // http://en.wikipedia.org/wiki/PostgreSQL
-  //
-  // Run:
-  // npm install sails-postgresql
-  //
-  somePostgresqlServer: {
-    adapter   : 'sails-postgresql',
-    host      : 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user      : 'YOUR_POSTGRES_USER',
-    password  : 'YOUR_POSTGRES_PASSWORD', 
-    database  : 'YOUR_POSTGRES_DB'
   }
-
-
-  // More adapters:
-  // https://github.com/balderdashy/sails
-
 };
 
 
