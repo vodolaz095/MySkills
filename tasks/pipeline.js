50621e2abbc56fc1522b197a5ec7be9b6b96af56
Ronen Teva <ronenteva@gmail.com>
2014-04-08 15:04:04 +0300

    fixing css issues

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -15,8 +15,8 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-//    'bower_components/bootstrap/dist/css/bootstrap.min.css',
-//    'bower_components/components-font-awesome/css/font-awesome.min.css',
+    'bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'bower_components/components-font-awesome/css/font-awesome.min.css',
     'styles/**/*.css'
 ];
 
