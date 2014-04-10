4f419e9b6f80dbababbeb5d8e2f5dc3a930ee7a5
Ronen Teva <ronenteva@gmail.com>
2014-04-10 22:40:06 +0300

    removed seo4ajax

--- api/controllers/GeneralController.js
+++ api/controllers/GeneralController.js
@@ -1,14 +1,13 @@
 module.exports = {
     angular: function (req, res, next) {
-        if (req.query._escaped_fragment_)
-        {
+//        if (req.query._escaped_fragment_)
+//        {
             //res.redirect('http://api.seo4ajax.com/53be25f061015577115854093f7ec5a9/?_escaped_fragment_=' + req.query._escaped_fragment_);
 //            res.redirect('/snapshots' + req.query._escaped_fragment_);
-            res.redirect('http://api.seo4ajax.com/53be25f061015577115854093f7ec5a9' + req.query._escaped_fragment_);
-        }else{
+//            res.redirect('http://api.seo4ajax.com/53be25f061015577115854093f7ec5a9' + req.query._escaped_fragment_);
+//        }else{
             res.view('angular');
-        }
-
+//        }
     },
     snapshots:function(req,res,next){
         res.send(req.headers['user-agent']);
